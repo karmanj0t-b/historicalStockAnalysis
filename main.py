6bde84d70f292e37aacfbe578d3a3e482151b7c7
Karmanjot B. <karmanjotbaidwan@gmail.com>
2026-09-13 18:05:27 -0400

    updated code

--- main.py
+++ main.py
@@ -44,6 +44,35 @@ def stock_volatility(stock_data):
     """
     return stock_data['Close'].std()
 
+def stock_return(stock_data):
+    """
+    Calculate the return of the stock's closing prices.
+
+    Parameters:
+    stock_data (pd.DataFrame): A DataFrame containing the historical stock data.
+
+    Returns:
+    float: The return of the stock's closing prices.
+    """
+    return (stock_data['Close'][-1] - stock_data['Close'][0]) / stock_data['Close'][0]
+
+
+def open_earliest_to_close_latest_difference(stock_data):
+    """
+    Calculate the difference between the opening price on the earliest date
+    and the closing price on the latest date.
+
+    Parameters:
+    stock_data (pd.DataFrame): A DataFrame containing the historical stock data.
+
+    Returns:
+    float: The difference between the earliest Open and latest Close.
+    """
+    earliest_open = stock_data['Open'].iloc[0]
+    latest_close = stock_data['Close'].iloc[-1]
+    return latest_close - earliest_open
+
+
 def __main__():
     rbc_data = get_stock_data('RY.TO', '2020-01-01', '2023-01-01')
     td_data = get_stock_data('TD.TO', '2020-01-01', '2023-01-01')
@@ -56,6 +85,8 @@ def __main__():
     for x in data_list:
         print(f"Variance: {stock_variance(x)}")
         print(f"Volatility: {stock_volatility(x)}")
+        print(f"Open at earliest date vs Close at latest date difference: {open_earliest_to_close_latest_difference(x)}")
+        #print(f"Return: {stock_return(x)}")
 
 
 if __name__ == "__main__":
